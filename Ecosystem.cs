3c0b552a6a523aa810142119cf96e77f59407a55
jxz12 <jxz12@ic.ac.uk>
2016-02-12 20:15:55 +0000

    added initial c# interface for Ecosystem

---
+++ Ecosystem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+
+interface Ecosystem {
+	void AddSpecies(string name, float selfInteraction, float birthRate, float population=0f);
+	void AddSpecies(string name, float m_i, float b_0, float d_0, float a_ii0, float gamma=0.5f, float beta=0.75f, float population=0f);
+	void RemoveSpecies(string name);
+	
+	void AddInteraction(string predator, string prey, float interactionRate, float conversionEfficiency);
+	void AddInteraction(string predator, string prey, float a_0, float e, float s, float k);
+
+	void IntegrateFirstOrder(float timestep);
+	void IntegrateRungeKutta(float timestep);
+
+	float GetPopulation(string name);
+	Dictionary<string, float> GetPopulations(List<string> names);
+	Dictionary<string, float> GetAllPopulations();
+
+	void SetPopulation(string name, float population);
+	void ChangePopulation(string name, float amount);
+}
